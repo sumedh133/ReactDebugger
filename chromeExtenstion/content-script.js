7149a0bbb7e5b82089ad30e61349dacb961ed2eb
Smedh123 <sumedh@truestate.in>
2025-07-04 12:41:32 +0530

    cleaned

--- chromeExtenstion/content-script.js
+++
@@ -1,6 +0,0 @@
-console.log("🚀 content-script.js loaded (isolated world)");
-
-const script = document.createElement("script");
-script.src = chrome.runtime.getURL("injected.js");
-script.onload = () => script.remove();
-(document.head || document.documentElement).appendChild(script);
