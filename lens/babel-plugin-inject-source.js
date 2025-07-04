69f108d95f492918a55c2f80bc098a359fa69b49
Smedh123 <sumedh@truestate.in>
2025-07-04 10:35:10 +0530

    chenged to absolute paths and file opening

--- lens/babel-plugin-inject-source.js
+++ lens/babel-plugin-inject-source.js
@@ -8,12 +8,11 @@ const injectSource = ({ types: t }) => {
         const filename = state.file.opts.filename;
         if (!filename || filename.includes("node_modules")) return;
 
-        // ✅ Inject only into DOM tags (lowercase like div, span)
         const tag = pathNode.node.name;
         if (!t.isJSXIdentifier(tag)) return;
-        if (!/^[a-z]/.test(tag.name)) return; // skip React components and fragments
+        if (!/^[a-z]/.test(tag.name)) return; // skip React components
 
-        const relPath = path.relative(process.cwd(), filename);
+        const absPath = path.resolve(filename);
         const line = pathNode.node.loc?.start.line;
         if (!line) return;
 
@@ -25,7 +24,7 @@ const injectSource = ({ types: t }) => {
         pathNode.node.attributes.push(
           t.jsxAttribute(
             t.jsxIdentifier("data-source"),
-            t.stringLiteral(`${relPath}:${line}`)
+            t.stringLiteral(`${absPath}:${line}`)
           )
         );
       },
